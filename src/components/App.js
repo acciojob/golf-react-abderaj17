32f18124c4f526a17624c61b31e29c82421090e1
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2024-11-28 15:53:23 +0530

    com

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
-
+ 
 class App extends Component {
     constructor(props) {
         super(props)
@@ -11,24 +11,29 @@ class App extends Component {
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
+ 
     };
-
+ 
     buttonClickHandler() {
-   
+        this.setState({...this.state, renderBall: true});
    }
     renderBallOrButton() {
 		if (this.state.renderBall) {
 		    return <div className="ball" style={this.state.ballPosition}></div>
 		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
+		    return <button className="start" onClick={this.buttonClickHandler} >Start</button>
 		}
     }
-
+ 
     // bind ArrowRight keydown event
     componentDidMount() {
-      
+      document.addEventListener('keydown', (event) => {
+        if (event.key === 'ArrowRight' || event.keyCode === 39) {
+            this.setState({...this.state, ballPosition: {left: parseInt(this.state.ballPosition.left, 10)+5+"px"}});
+        }
+      });
     }
-
+ 
     render() {
         return (
             <div className="playground">
@@ -37,6 +42,6 @@ class App extends Component {
         )
     }
 }
-
-
+ 
+ 
 export default App;
